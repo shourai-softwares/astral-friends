8193479da40d4d57f565e658bea03bc936f94546
Duilio Felix <duilio@lipperhub.com>
2018-12-20 07:17:48 -0200

    Adding base map screen

--- src/components/UserPortrait.js
+++ src/components/UserPortrait.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ImageBackground, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import styled from 'styled-components/native';
+import { SIGN_IMAGES } from '../assets';
+
+const RoundImage = styled.Image`
+  border-radius: 50;
+  overflow: hidden;
+`;
 
 const UserPortrait = ({ style }) => (
   <View style={style}>
-    <ImageBackground
-      style={{ flexDirection:'row', alignItems: 'flex-end', width: '100%', height: '100%' }}
+    <RoundImage
+      style={{ width: 100, height: 100 }}
       source={{ uri: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png' }}
-    >
-      <View style={{ flexGrow: 1, alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.3)', padding: 3 }}>
-        <Text style={{ color: 'white' }}>jajajaj</Text>
-      </View>
-    </ImageBackground>
+    />
+    <RoundImage
+      style={{ position: 'absolute', width: 30, height: 30 }}
+      source={SIGN_IMAGES.SCORPIO}
+    />
+    <View style={{ flexGrow: 1, alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.3)', padding: 3 }}>
+      <Text style={{ color: 'white' }}>jajajaj</Text>
+    </View>
   </View>
 );
 UserPortrait.propType = {
   style: PropTypes.object,
 };
 
-export default styled(UserPortrait)`
-  overflow: hidden;
-  width: 100;
-  height: 100;
-  border-radius: 50;
-  border-color: black;
-  border-width: 1;
-`;
+export default UserPortrait;
--- src/containers/MapScreen.js
+++ src/containers/MapScreen.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Image, View, Text } from 'react-native';
+import { SIGN_IMAGES } from '../assets';
 
 const MapScreen = () => (
-  <View>
-    <Text>Olá</Text>
+  <View style={{ padding: 10 }}>
+    <View style={{ flexDirection: 'row' }}>
+      <View style={{ borderColor: 'orange', borderWidth: 1, flexGrow: 1, alignItems: 'center' }}>
+        <Text>Meu Mapa</Text>
+      </View>
+      <View style={{ backgroundColor: 'orange', borderColor: 'orange', borderWidth: 1, alignItems: 'center', flexGrow: 1 }}>
+        <Text>Meus Amigos</Text>
+      </View>
+    </View>
+    <View style={{ alignItems: 'center', marginTop: 10 }}>
+      <View>
+        <Image source={SIGN_IMAGES.VIRGIN} style={{ width: 200, height: 200 }} />
+      </View>
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+        <View>
+          <Text>Lua</Text>
+          <Image source={SIGN_IMAGES.SCORPIO} style={{ width: 100, height: 100 }} />
+        </View>
+        <View>
+          <Text>Vênus</Text>
+          <Image source={SIGN_IMAGES.TWINS} style={{ width: 100, height: 100 }} />
+        </View>
+        <View>
+          <Text>Marte</Text>
+          <Image source={SIGN_IMAGES.LION} style={{ width: 100, height: 100 }} />
+        </View>
+        <View>
+          <Text>Clique em qualquer astro para mais informações</Text>
+          <Text>GRANDE TEXTO RESUMO DO MAPA</Text>
+        </View>
+      </View>
+    </View>
   </View>
 );
 
